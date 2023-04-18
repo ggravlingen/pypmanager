78fd3646c38374a4eb0f386ec2d4b1da65cdde85
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2023-04-18 19:49:03 +0200

    Bootstrap (#1)

---
+++ script/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Resolve internal dependencies that the application requires to run.
+
+# Stop on errors
+set -e
+
+cd "$(dirname "$0")/../"
+
+echo "Installing internal dependencies..."
+
+python3 -m pip install --upgrade pip setuptools wheel cython
+python3 -m pip install -e .
+python3 -m pip install -r requirements_test.txt
