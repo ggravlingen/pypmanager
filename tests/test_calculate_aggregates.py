8b98f497308a71850cb333b76dfa91ee57a55d24
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2023-06-09 21:24:12 +0200

    Add test (#89)

--- tests/test_calculate_aggregates.py
+++ tests/test_calculate_aggregates.py
@@ -44,3 +44,38 @@ def test_interest_transaction():
     data = CalculateAggregates(security_transactions=df_test)
 
     assert_frame_equal(data.output_data, expected_df)
+
+
+def test_dividend_transaction():
+    """Test interest transaction."""
+    expected_df = pd.DataFrame(
+        [
+            {
+                ColumnNameValues.AMOUNT: 100.0,
+                ColumnNameValues.BROKER: "Broker",
+                ColumnNameValues.NAME: "Name A",
+                ColumnNameValues.REALIZED_PNL: 100.0,
+                ColumnNameValues.REALIZED_PNL_DIVIDEND: 100.0,
+                ColumnNameValues.REALIZED_PNL_INTEREST: 0.0,
+                ColumnNameValues.SOURCE: "Source",
+                ColumnNameValues.TRANSACTION_DATE: datetime(2023, 4, 1),
+                ColumnNameValues.TRANSACTION_TYPE: TransactionTypeValues.DIVIDEND,
+            }
+        ]
+    )
+    expected_df = expected_df.set_index(ColumnNameValues.TRANSACTION_DATE)
+
+    data = [
+        {
+            ColumnNameValues.AMOUNT: 100.0,
+            ColumnNameValues.BROKER: "Broker",
+            ColumnNameValues.NAME: "Name A",
+            ColumnNameValues.SOURCE: "Source",
+            ColumnNameValues.TRANSACTION_DATE: datetime(2023, 4, 1),
+            ColumnNameValues.TRANSACTION_TYPE: TransactionTypeValues.DIVIDEND,
+        }
+    ]
+    df_test = pd.DataFrame(data)
+    df_test = df_test.set_index(ColumnNameValues.TRANSACTION_DATE)
+
+    data = CalculateAggregates(security_transactions=df_test)
