3f3d47cedec5e44b205dfe1db73b4b1301c87418
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2023-07-26 21:04:02 +0200

    Add architecture information (#153)

--- pypmanager/server/__init__.py
+++ pypmanager/server/__init__.py
@@ -1,21 +1,15 @@
 """Server."""
 from __future__ import annotations
 
-import time
-from typing import cast
-
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, HTMLResponse
 
 from pypmanager.analytics import Portfolio
-from pypmanager.analytics.holding import Holding
-from pypmanager.helpers import get_general_ledger
+from pypmanager.helpers import get_general_ledger, get_holdings
 from pypmanager.loader_transaction.const import ColumnNameValues
 from pypmanager.server.graphql import graphql_app
 from pypmanager.server.templates import load_template
 from pypmanager.settings import Settings
-
-from .const import LOGGER
 
 app = FastAPI()
 
@@ -27,37 +21,9 @@ async def get_favicon() -> FileResponse:
 
 
 @app.get("/", response_class=HTMLResponse)
-async def index(view: str | None = None) -> str:
+async def index() -> str:
     """Present overview page."""
-    df_general_ledger = get_general_ledger()
-    all_securities = cast(list[str], df_general_ledger[ColumnNameValues.NAME].unique())
-
-    holdings: list[Holding] = []
-    for security_name in all_securities:
-        start_time = time.time()
-
-        holding = Holding(
-            name=security_name,
-            df_general_ledger=df_general_ledger,
-        )
-
-        end_time = time.time()
-
-        LOGGER.info(
-            f"Calculated {security_name} in {round((end_time - start_time), 4)}s"
-        )
-
-        if view is None and holding.current_holdings is None:
-            LOGGER.debug(f"Skipping {holding}")
-            continue
-
-        if view == "old" and holding.current_holdings is not None:
-            continue
-
-        holdings.append(holding)
-
-    # Order by name
-    holdings = sorted(holdings, key=lambda x: x.name)
+    holdings = await get_holdings()
 
     portfolio = Portfolio(holdings=holdings)
 
