511e9ccee98988a6f059d08fd16812e4b16c912e
Patrik <21142447+ggravlingen@users.noreply.github.com>
2024-02-25 09:24:25 +0000

    Add tests for settings

---
+++ tests/test_settings.py
@@ -0,0 +1,28 @@
+"""Tests for settings."""
+
+import os
+
+from pypmanager.settings import TypedSettings
+
+
+def test_dir_data_demo():
+    """Test property dir_data in demo mode."""
+    settings = TypedSettings()
+    settings.is_demo = True
+    assert settings.dir_data == os.path.abspath("data-demo")
+
+
+def test_dir_data_not_demo():
+    """Test property dir_data in production mode."""
+    settings = TypedSettings()
+    settings.is_demo = False
+    assert settings.dir_data == os.path.abspath("data")
+
+
+def test_file_market_data_config():
+    """Test property file_market_data_config."""
+    settings = TypedSettings()
+    expected_path = os.path.abspath(
+        os.path.join(settings.dir_config, "market_data.yaml")
+    )
+    assert settings.file_market_data_config == expected_path
