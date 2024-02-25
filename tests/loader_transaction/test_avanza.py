f009bcb63283409647dd102d8b69ee24ab7cb79a
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-02-25 10:35:59 +0100

    Increase coverage in AvanzaLoader (#333)

--- tests/loader_transaction/test_avanza.py
+++ tests/loader_transaction/test_avanza.py
@@ -7,8 +7,15 @@ from pypmanager.settings import TypedSettings
 
 
 @patch.object(TypedSettings, "dir_data", "tests/fixtures/")
-def test_avanza_loder() -> None:
+def test_avanza_loader() -> None:
     """Test AvanzaLoader."""
+    loader = AvanzaLoader()
+
+    # Test the pre_process_df method
+    loader.pre_process_df()
+
+    assert "Resultat" not in loader.df_final.columns
+
     df_avanza = AvanzaLoader().df_final
 
     assert len(df_avanza) > 0
