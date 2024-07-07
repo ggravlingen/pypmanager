539726dbb15523a957324877e9103da80caf76a8
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-07-07 14:24:37 +0200

    Improve tsx config (#491)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,15 @@
 import App from "./App";
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 if (rootElement) {
   const root = (ReactDOM as any).createRoot(rootElement);
   root.render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
+    </React.StrictMode>,
   );
 } else {
   console.error("Root element 'root' not found in the document.");
