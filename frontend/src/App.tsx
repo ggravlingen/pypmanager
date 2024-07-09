7eb839f219f6375cc039ec7355f9f85cd72d59b0
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-07-09 21:12:06 +0200

    Improvements to frontend (#516)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import { Box } from "@mui/material";
 import { NavigationBar, TableGeneralLedger } from "./components";
 import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 export default function MainLayout() {
   return (
-    <Box sx={{ display: "flex" }}>
-      <NavigationBar />
-      <Box style={{ flexGrow: 1 }}>
-        <TableGeneralLedger />
+    <HashRouter>
+      <Box sx={{ display: "flex" }}>
+        <NavigationBar />
+        <Box style={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<TableGeneralLedger />} />
+          </Routes>
+        </Box>
       </Box>
-    </Box>
+    </HashRouter>
   );
 }
