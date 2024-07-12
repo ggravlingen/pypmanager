daa5bf562835c04e64af480b21c3af95751c647f
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-07-12 20:54:55 +0200

    Refactor market data config (#557)

--- pypmanager/ingest/market_data/__init__.py
+++ pypmanager/ingest/market_data/__init__.py
@@ -1,5 +1,14 @@
 """Market data ingestion code."""
 
+from .avanza import AvanzaLoader
+from .ft import FTLoader
 from .helpers import async_download_market_data
+from .morningstar import MorningstarLoader, MorningstarLoaderSHB
 
-__all__ = ["async_download_market_data"]
+__all__ = [
+    "async_download_market_data",
+    "AvanzaLoader",
+    "FTLoader",
+    "MorningstarLoader",
+    "MorningstarLoaderSHB",
+]
