7ddfe0a623d2d3872eafb995c95733b9ff173592
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-07-13 14:50:28 +0200

    Pl improve grpahql test (#562)

--- tests/api/test_endpoint.py
+++ tests/api/test_endpoint.py
@@ -1,4 +1,4 @@
-"""Tests for FastAPI server."""
+"""Tests for FastAPI server endpoints."""
 
 from fastapi.testclient import TestClient
 import pytest
@@ -19,20 +19,4 @@ async def test_root_endpoint() -> None:
 async def test_favicon() -> None:
     """Test endpoint /favicon.ico."""
     response = client.get("/favicon.ico")
-    assert response.status_code == 200
-
-
-@pytest.mark.asyncio()
-async def test_graphql_endpoint() -> None:
-    """Test endpoint /graphql."""
-    query = """
-    {
-        allGeneralLedger {
-            date
-            broker
-            source
-        }
-    }
-    """
-    response = client.post("/graphql", json={"query": query})
     assert response.status_code == 200
