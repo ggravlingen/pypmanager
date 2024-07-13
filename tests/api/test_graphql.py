9ede48201af60add4d74f34c95ebcc77e72d6d07
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-07-13 15:00:59 +0200

    Add more tests for GraphQL (#563)

--- tests/api/test_graphql.py
+++ tests/api/test_graphql.py
@@ -1,10 +1,12 @@
 """Tests for graphql."""
 
 from collections.abc import Callable, Generator
+from datetime import date
 from typing import Any
 from unittest.mock import patch
 
 from fastapi.testclient import TestClient
+from freezegun.api import FrozenDateTimeFactory
 import pandas as pd
 import pytest
 
@@ -20,9 +22,18 @@ def _mock_transaction_list(
     """Mock the transaction list."""
     mocked_transactions = df_transaction_data_factory(no_rows=1)
 
-    with patch(
-        "pypmanager.general_ledger.helpers.load_transaction_files",
-        return_value=mocked_transactions,
+    # make the transaction_date field into the index
+    mocked_transactions.index = mocked_transactions["transaction_date"]
+
+    with (
+        patch(
+            "pypmanager.general_ledger.helpers.load_transaction_files",
+            return_value=mocked_transactions,
+        ),
+        patch(
+            "pypmanager.api.graphql.query.load_transaction_files",
+            return_value=mocked_transactions,
+        ),
     ):
         yield
 
@@ -30,7 +41,7 @@ def _mock_transaction_list(
 @pytest.mark.asyncio()
 @pytest.mark.usefixtures("_mock_transaction_list")
 async def test_graphql_query__all_general_ledger() -> None:
-    """Test endpoint /graphql."""
+    """Test query allGeneralLedger."""
     query = """
     {
         allGeneralLedger {
@@ -56,3 +67,77 @@ async def test_graphql_query__all_general_ledger() -> None:
     response = client.post("/graphql", json={"query": query})
     assert response.status_code == 200
     assert len(response.json()["data"]["allGeneralLedger"]) == 2
+
+
+@pytest.mark.asyncio()
+@pytest.mark.usefixtures("_mock_transaction_list")
+async def test_graphql_query__current_portfolio() -> None:
+    """Test query currentPortfolio."""
+    query = """
+    {
+        currentPortfolio {
+            name
+            dateMarketValue
+            investedAmount
+            marketValue
+            currentHoldings
+            currentPrice
+            averagePrice
+            returnPct
+            totalPnl
+            realizedPnl
+            unrealizedPnl
+        }
+    }
+    """
+    response = client.post("/graphql", json={"query": query})
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio()
+@pytest.mark.usefixtures("_mock_transaction_list")
+async def test_graphql_query__historical_portfolio(
+    freezer: FrozenDateTimeFactory,
+) -> None:
+    """Test query historicalPortfolio."""
+    freezer.move_to(date(2023, 8, 5))
+
+    query = """
+    {
+        historicalPortfolio {
+            reportDate
+            investedAmount
+            marketValue
+            returnPct
+            realizedPnl
+            unrealizedPnl
+        }
+    }
+    """
+    response = client.post("/graphql", json={"query": query})
+    assert response.status_code == 200
+    assert len(response.json()["data"]["historicalPortfolio"]) == 9
+
+
+@pytest.mark.asyncio()
+@pytest.mark.usefixtures("_mock_transaction_list")
+async def test_graphql_query__all_transaction() -> None:
+    """Test query allTransaction."""
+    query = """
+    {
+        allTransaction {
+            transactionDate
+            broker
+            source
+            action
+            name
+            noTraded
+            price
+            commission
+            fx
+        }
+    }
+    """
+    response = client.post("/graphql", json={"query": query})
+    assert response.status_code == 200
+    assert len(response.json()["data"]["allTransaction"]) == 1
