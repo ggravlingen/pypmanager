c279c7e8577546a89ed4a36de9e254327dc990fc
Patrik <21142447+ggravlingen@users.noreply.github.com>
2024-07-13 20:45:29 +0000

    Improve the server.js

---
+++ frontend/tests/server.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+const express = require("express");
+const path = require("path");
+const http = require("http");
+
+// Paths
+const FRONTEND_DIR = path.join(__dirname, "../");
+const TEMPLATE_DIR = path.join(FRONTEND_DIR, "templates");
+const STATIC_DIR = path.join(FRONTEND_DIR, "static");
+
+const app = express();
+const server = http.createServer(app);
+
+// Middleware for logging requests
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} request to ${req.url}`);
+  next(); // Continue to the next middleware or route handler
+});
+
+// Serving the files in the static folder
+app.use('/static', express.static(STATIC_DIR));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(TEMPLATE_DIR, "index.html"), (e) => {
+    if (e) {
+      console.log("Error sending file:", e);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ frontend/webpack/playwright.tsx
@@ -0,0 +1,15 @@
+import path from "path";
+import { merge } from "webpack-merge";
+
+import commonConfig from "./common";
+
+
+export default merge(commonConfig, {
+  entry: path.resolve(__dirname, "../src/index.tsx"),
+  mode: "development",
+  output: {
+    clean: true,
+    path: path.resolve(__dirname, "static"),
+    filename: "main.js",
+  },
+});
