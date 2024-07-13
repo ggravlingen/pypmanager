62ea2bee327e8f957b79642eb0558e786a28c9ed
Patrik <21142447+ggravlingen@users.noreply.github.com>
2024-07-13 11:59:18 +0000

    Order transaction listt descending

--- pypmanager/ingest/transaction/helpers.py
+++ pypmanager/ingest/transaction/helpers.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING
 
 import pandas as pd
 
@@ -14,15 +14,19 @@ if TYPE_CHECKING:
     from datetime import datetime
 
 
-def load_transaction_files(report_date: datetime | None = None) -> pd.DataFrame:
-    """Load all transaction files into a Dataframe."""
-    return cast(
-        pd.DataFrame,
-        pd.concat(
-            [
-                GenericLoader(report_date).df_final,
-                AvanzaLoader(report_date).df_final,
-                LysaLoader(report_date).df_final,
-            ],
-        ),
-    )
+def load_transaction_files(
+    report_date: datetime | None = None,
+    *,
+    sort_by_date_descending: bool = False,
+) -> pd.DataFrame:
+    """Load transaction files and return a sorted DataFrame."""
+    df_generic = GenericLoader(report_date).df_final
+    df_avanza = AvanzaLoader(report_date).df_final
+    df_lysa = LysaLoader(report_date).df_final
+
+    df_combined = pd.concat([df_generic, df_avanza, df_lysa])
+
+    if sort_by_date_descending:
+        return df_combined.sort_index(ascending=False)
+
+    return df_combined
--- pypmanager/settings.py
+++ pypmanager/settings.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from pathlib import Path
+from zoneinfo import ZoneInfo
 
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -32,6 +33,8 @@ class TypedSettings(BaseSettings):
     dir_config: Path = Path("pypmanager/configuration").resolve()
     dir_static: Path = Path("frontend/static").resolve()
     dir_templates: Path = Path("frontend/templates").resolve()
+
+    system_time_zone: ZoneInfo = ZoneInfo("Europe/Stockholm")
 
     @property
     def dir_market_data(self: TypedSettings) -> Path:
