883322cb8fea658c6905384d13494e98ec45492c
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-07-15 20:58:32 +0200

    Add useQueryGetIncomeStatement (#584)

--- frontend/src/Api/Models/index.tsx
+++ frontend/src/Api/Models/index.tsx
@@ -1,10 +1,12 @@
 import { HistoricalPortfolioRow } from "./history";
 import { LedgerRow, TransactionRow } from "./ledger";
 import { PortfolioContentRow } from "./portfolio";
+import { ResultStatementRow } from "./resultStatement";
 
 export {
   HistoricalPortfolioRow,
   LedgerRow,
   PortfolioContentRow,
+  ResultStatementRow,
   TransactionRow,
 };
