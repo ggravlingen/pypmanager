968c9c79507ff4f4d5f528d6a240a0d279f167e1
Patrik <21142447+ggravlingen@users.noreply.github.com>
2024-10-13 06:46:47 +0000

    Update README.md

---
+++ script/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Resolve internal dependencies that the application requires to run.
+
+# Stop on errors
+set -e
+
+cd "$(dirname "$0")/../"
+
+# Install backend
+echo "Installing backend"
+python3 -m pip install -e .'[test]'
+
+# Instasll frontend
+echo "Installing frontend"
+cd frontend
+yarn
+
+# Build the frontend
+yarn build-prod
