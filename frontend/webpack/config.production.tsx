fd4184ace0afc5eff7cdf12f4213abf13c0ffaf2
Patrik <21142447+ggravlingen@users.noreply.github.com>
2024-12-20 12:23:08 +0000

    Improve webpack

--- frontend/webpack/config.production.tsx
+++ frontend/webpack/config.production.tsx
@@ -1,4 +1,6 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import * as path from "path";
+import TerserPlugin from "terser-webpack-plugin";
 import { merge } from "webpack-merge";
 
 import commonConfig from "./common";
@@ -7,6 +9,32 @@ export default merge(commonConfig, {
   mode: "production",
   output: {
     path: path.join(__dirname, "..", "static"),
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
+    chunkFilename: "[name].[contenthash].js",
   },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        compress: { drop_console: true },
+        format: { comments: false },
+      },
+      extractComments: false,
+    })],
+    splitChunks: {
+      chunks: "all",
+      automaticNameDelimiter: "-",
+    },
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "..", "templates", "index_template.html"),
+      filename: "./../templates/index.html",
+    }),
+  ],
 });
