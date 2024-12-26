4c1235010fe84384250dace8efac03fda3f738a9
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-12-26 20:06:57 +0100

    Improve SecurityHoldingHistory (#1012)

--- tests/helpers/test_security_holding_history.py
+++ tests/helpers/test_security_holding_history.py
@@ -28,7 +28,9 @@ def mock_security_transaction_registry(
         .buy(transaction_date=datetime(2022, 11, 2, tzinfo=Settings.system_time_zone))
         .buy(transaction_date=datetime(2022, 11, 3, tzinfo=Settings.system_time_zone))
         .buy(transaction_date=datetime(2022, 11, 4, tzinfo=Settings.system_time_zone))
-        .sell(transaction_date=datetime(2022, 11, 8, tzinfo=Settings.system_time_zone))
+        .sell(transaction_date=datetime(2022, 12, 8, tzinfo=Settings.system_time_zone))
+        .sell(transaction_date=datetime(2022, 12, 9, tzinfo=Settings.system_time_zone))
+        .sell(transaction_date=datetime(2022, 12, 10, tzinfo=Settings.system_time_zone))
         .df_transaction_list
     )
     with patch(
@@ -45,7 +47,7 @@ async def test_security_holding_history__transaction_list() -> None:
     transaction_registry = await TransactionRegistry().async_get_registry()
     shh = SecurityHoldingHistory("US1234567890", transaction_registry)
 
-    assert len(shh.transaction_list) == 4
+    assert len(shh.transaction_list) == 6
 
 
 @pytest.mark.usefixtures("mock_security_transaction_registry")
@@ -57,6 +59,46 @@ async def test_security_holding_history__series_date_range() -> None:
 
     # Test the series_date_range property
     assert shh.series_date_range is not None
-    assert len(shh.series_date_range) == 5
+    assert len(shh.series_date_range) == 40
     assert shh.series_date_range[0] == shh.series_start_date
     assert shh.series_date_range[-1] == shh.series_end_date
+
+
+@pytest.mark.usefixtures("mock_security_transaction_registry")
+@pytest.mark.asyncio
+async def test_security_holding_history__df_base() -> None:
+    """Test SecurityHoldingHistory.df_base."""
+    transaction_registry = await TransactionRegistry().async_get_registry()
+    shh = SecurityHoldingHistory("US1234567890", transaction_registry)
+
+    assert shh.df_base is not None
+    assert len(shh.df_base) == 40
+    assert shh.df_base.index[0] == shh.series_start_date
+    assert shh.df_base.index[-1] == shh.series_end_date
+
+
+@pytest.mark.usefixtures("mock_security_transaction_registry")
+@pytest.mark.asyncio
+async def test_security_holding_history__df_base_with_transactions() -> None:
+    """Test SecurityHoldingHistory.df_base_with_transactions."""
+    transaction_registry = await TransactionRegistry().async_get_registry()
+    shh = SecurityHoldingHistory("US1234567890", transaction_registry)
+
+    assert shh.df_base_with_transactions is not None
+    assert len(shh.df_base_with_transactions) == 40
+    assert shh.df_base.index[0] == shh.series_start_date
+    assert shh.df_base.index[-1] == shh.series_end_date
+
+
+@pytest.mark.usefixtures("mock_security_transaction_registry")
+@pytest.mark.asyncio
+async def test_security_holding_history__df_transaction_clean() -> None:
+    """Test SecurityHoldingHistory.df_base_with_transactions."""
+    transaction_registry = await TransactionRegistry().async_get_registry()
+    shh = SecurityHoldingHistory("US1234567890", transaction_registry)
+
+    assert shh.df_transaction_clean is not None
+    assert len(shh.df_transaction_clean) == 40
+
+    assert shh.df_base.index[0] == shh.series_start_date
+    assert shh.df_base.index[-1] == shh.series_end_date
