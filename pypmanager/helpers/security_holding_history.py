0e9d97ef55bfb6d23de2130e7614733320333dea
Patrik <21142447+ggravlingen@users.noreply.github.com>
2024-12-26 20:46:53 +0000

    Improve calculations

--- pypmanager/helpers/security_holding_history.py
+++ pypmanager/helpers/security_holding_history.py
@@ -2,9 +2,11 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass
 from functools import cached_property
-from typing import ClassVar
+from typing import TYPE_CHECKING, ClassVar, cast
 
+import numpy as np
 import pandas as pd
 
 from pypmanager.ingest.transaction.const import (
@@ -12,6 +14,50 @@ from pypmanager.ingest.transaction.const import (
     TransactionRegistryColNameValues,
 )
 from pypmanager.settings import Settings
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+
+@dataclass
+class ColumnAppendConfig:
+    """Configuration to append columns."""
+
+    column: str
+    callable: Callable[[pd.DataFrame], pd.DataFrame]
+
+
+class SecurityHoldingHistoryPandasAlgorithm:
+    """Pandas algorithms for SecurityHoldingHistory class."""
+
+    @staticmethod
+    def clean_calc_agg_sum_quantity_held(row: pd.Series) -> float | None:
+        """Test."""
+        if row[TransactionRegistryColNameValues.ADJUSTED_QUANTITY_HELD.value] > 0:
+            return cast(
+                float,
+                row[TransactionRegistryColNameValues.PRICE_PER_UNIT.value],
+            )
+
+        # Everything has been sold, just return 0
+        if (
+            row[
+                TransactionRegistryColNameValues.CALC_ADJUSTED_QUANTITY_HELD_IS_RESET.value
+            ]
+            is True
+        ):
+            return 0.0
+
+        return None
+
+
+# These columns are appended to the transaction registry
+COLUMN_APPEND: tuple[ColumnAppendConfig, ...] = (
+    ColumnAppendConfig(
+        column=TransactionRegistryColNameValues.PRICE_PER_UNIT.value,
+        callable=SecurityHoldingHistoryPandasAlgorithm.clean_calc_agg_sum_quantity_held,
+    ),
+)
 
 
 class SecurityHoldingHistory:
@@ -41,14 +87,24 @@ class SecurityHoldingHistory:
         ColumnNameValues.AMOUNT,
     ]
 
+    COLS_TO_DROP_LEVEL_2: ClassVar = [
+        TransactionRegistryColNameValues.ADJUSTED_QUANTITY_HELD,
+        TransactionRegistryColNameValues.CALC_ADJUSTED_QUANTITY_HELD_IS_RESET.value,
+    ]
+
     df_base: pd.DataFrame
     df_base_with_transactions: pd.DataFrame
     df_transaction_clean: pd.DataFrame
+    df_transaction_filled: pd.DataFrame
 
-    def __init__(self, isin_code: str, df_transaction_registry: pd.DataFrame) -> None:
+    def __init__(
+        self,
+        *,
+        isin_code: str,
+        df_transaction_registry: pd.DataFrame,
+    ) -> None:
         """Init class."""
         self.isin_code = isin_code
-
         # Filter transactions for ISIN code
         self.transaction_list = df_transaction_registry[
             df_transaction_registry[TransactionRegistryColNameValues.SOURCE_ISIN]
@@ -62,6 +118,7 @@ class SecurityHoldingHistory:
         self._100_create_base_df()
         self._200_merge_transaction_list()
         self._300_drop_columns()
+        self._400_fix_missing_values()
 
     @cached_property
     def series_date_range(self) -> pd.Series:
@@ -90,3 +147,22 @@ class SecurityHoldingHistory:
         self.df_transaction_clean = self.df_base_with_transactions.drop(
             columns=self.COLS_TO_DROP
         )
+
+    def _400_fix_missing_values(self) -> pd.DataFrame:
+        """Fix missing values."""
+        df_raw = self.df_transaction_clean.copy()
+
+        for config in COLUMN_APPEND:
+            df_raw[config.column] = df_raw.apply(config.callable, axis=1)
+
+        # Fill missing values
+        df_raw[TransactionRegistryColNameValues.PRICE_PER_UNIT.value] = df_raw[
+            TransactionRegistryColNameValues.PRICE_PER_UNIT.value
+        ].ffill()
+
+        df_raw = df_raw.drop(columns=self.COLS_TO_DROP_LEVEL_2)
+
+        # Fill NaN values with None
+        df_raw = df_raw.replace({np.nan: None})
+
+        self.df_transaction_filled = df_raw
--- tests/helpers/test_security_holding_history.py
+++ tests/helpers/test_security_holding_history.py
@@ -6,9 +6,11 @@ from datetime import datetime
 from typing import TYPE_CHECKING
 from unittest.mock import patch
 
+from numpy.testing import assert_array_equal
 import pytest
 
 from pypmanager.helpers.security_holding_history import SecurityHoldingHistory
+from pypmanager.ingest.transaction.const import TransactionRegistryColNameValues
 from pypmanager.ingest.transaction.transaction_registry import TransactionRegistry
 from pypmanager.settings import Settings
 
@@ -28,9 +30,10 @@ def mock_security_transaction_registry(
         .buy(transaction_date=datetime(2022, 11, 2, tzinfo=Settings.system_time_zone))
         .buy(transaction_date=datetime(2022, 11, 3, tzinfo=Settings.system_time_zone))
         .buy(transaction_date=datetime(2022, 11, 4, tzinfo=Settings.system_time_zone))
+        .sell(transaction_date=datetime(2022, 12, 4, tzinfo=Settings.system_time_zone))
         .sell(transaction_date=datetime(2022, 12, 8, tzinfo=Settings.system_time_zone))
         .sell(transaction_date=datetime(2022, 12, 9, tzinfo=Settings.system_time_zone))
-        .sell(transaction_date=datetime(2022, 12, 10, tzinfo=Settings.system_time_zone))
+        .buy(transaction_date=datetime(2022, 12, 10, tzinfo=Settings.system_time_zone))
         .df_transaction_list
     )
     with patch(
@@ -45,7 +48,10 @@ def mock_security_transaction_registry(
 async def test_security_holding_history__transaction_list() -> None:
     """Test SecurityHoldingHistory.series_date_range."""
     transaction_registry = await TransactionRegistry().async_get_registry()
-    shh = SecurityHoldingHistory("US1234567890", transaction_registry)
+    shh = SecurityHoldingHistory(
+        isin_code="US1234567890",
+        df_transaction_registry=transaction_registry,
+    )
 
     assert len(shh.transaction_list) == 6
 
@@ -55,7 +61,10 @@ async def test_security_holding_history__transaction_list() -> None:
 async def test_security_holding_history__series_date_range() -> None:
     """Test SecurityHoldingHistory.series_date_range."""
     transaction_registry = await TransactionRegistry().async_get_registry()
-    shh = SecurityHoldingHistory("US1234567890", transaction_registry)
+    shh = SecurityHoldingHistory(
+        isin_code="US1234567890",
+        df_transaction_registry=transaction_registry,
+    )
 
     # Test the series_date_range property
     assert shh.series_date_range is not None
@@ -69,7 +78,10 @@ async def test_security_holding_history__series_date_range() -> None:
 async def test_security_holding_history__df_base() -> None:
     """Test SecurityHoldingHistory.df_base."""
     transaction_registry = await TransactionRegistry().async_get_registry()
-    shh = SecurityHoldingHistory("US1234567890", transaction_registry)
+    shh = SecurityHoldingHistory(
+        isin_code="US1234567890",
+        df_transaction_registry=transaction_registry,
+    )
 
     assert shh.df_base is not None
     assert len(shh.df_base) == 40
@@ -82,7 +94,10 @@ async def test_security_holding_history__df_base() -> None:
 async def test_security_holding_history__df_base_with_transactions() -> None:
     """Test SecurityHoldingHistory.df_base_with_transactions."""
     transaction_registry = await TransactionRegistry().async_get_registry()
-    shh = SecurityHoldingHistory("US1234567890", transaction_registry)
+    shh = SecurityHoldingHistory(
+        isin_code="US1234567890",
+        df_transaction_registry=transaction_registry,
+    )
 
     assert shh.df_base_with_transactions is not None
     assert len(shh.df_base_with_transactions) == 40
@@ -95,10 +110,77 @@ async def test_security_holding_history__df_base_with_transactions() -> None:
 async def test_security_holding_history__df_transaction_clean() -> None:
     """Test SecurityHoldingHistory.df_base_with_transactions."""
     transaction_registry = await TransactionRegistry().async_get_registry()
-    shh = SecurityHoldingHistory("US1234567890", transaction_registry)
+    shh = SecurityHoldingHistory(
+        isin_code="US1234567890",
+        df_transaction_registry=transaction_registry,
+    )
 
     assert shh.df_transaction_clean is not None
     assert len(shh.df_transaction_clean) == 40
 
     assert shh.df_base.index[0] == shh.series_start_date
     assert shh.df_base.index[-1] == shh.series_end_date
+
+
+@pytest.mark.usefixtures("mock_security_transaction_registry")
+@pytest.mark.asyncio
+async def test_security_holding_history__df_transaction_filled() -> None:
+    """Test SecurityHoldingHistory.df_transaction_filled."""
+    transaction_registry = await TransactionRegistry().async_get_registry()
+    shh = SecurityHoldingHistory(
+        isin_code="US1234567890",
+        df_transaction_registry=transaction_registry,
+    )
+
+    assert shh.df_transaction_filled is not None
+    assert len(shh.df_transaction_filled) == 40
+
+    actual_average_price = shh.df_transaction_filled[
+        TransactionRegistryColNameValues.PRICE_PER_UNIT.value
+    ].to_numpy()
+    expected_average_price = [
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        10.0,
+        0.0,
+        10.0,
+        10.0,
+    ]
+    assert_array_equal(
+        actual_average_price,
+        expected_average_price,
+    )
