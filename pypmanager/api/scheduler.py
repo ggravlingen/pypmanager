6cfdc1d75f275c7b23180375a8ed4ac9d70b675f
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-12-27 20:19:07 +0100

    Run scheduler when starting server (#1036)

--- pypmanager/api/scheduler.py
+++ pypmanager/api/scheduler.py
@@ -1,4 +1,4 @@
-"""Main scheduler app."""
+"""Configure a scheduler."""
 
 from __future__ import annotations
 
@@ -13,10 +13,11 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pypmanager.helpers.market_data import async_download_market_data
 from pypmanager.settings import Settings
 
-LOGGER = logging.getLogger(__name__)
-
 if TYPE_CHECKING:
     from collections.abc import Callable, Coroutine
+
+LOGGER = logging.getLogger(__name__)
+
 
 DONE = asyncio.Event()
 
@@ -36,36 +37,20 @@ async def run_async_job(
     await job_func(*args, **kwargs)
 
 
-async def run_main() -> None:
-    """Run main script."""
-    LOGGER.info("Started scheduler")
-    scheduler = AsyncIOScheduler(
-        executors={
-            "threadpool": ThreadPoolExecutor(max_workers=1),
-        },
-        timezone=Settings.system_time_zone,
-    )
-    scheduler.add_listener(event_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
+scheduler = AsyncIOScheduler(
+    executors={
+        "threadpool": ThreadPoolExecutor(max_workers=1),
+    },
+    timezone=Settings.system_time_zone,
+)
+scheduler.add_listener(event_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
 
-    scheduler.add_job(
-        run_async_job,
-        id="load_market_data",
-        args=[async_download_market_data],
-        trigger="cron",
-        hour=21,
-        minute=0,
-        replace_existing=True,
-    )
-
-    scheduler.start()
-
-    await asyncio.sleep(1)  # Sleep for a short while to allow scheduler to start
-    scheduler.print_jobs()
-
-    # Keep the script running indefinitely
-    while True:
-        await DONE.wait()
-
-
-if __name__ == "__main__":
-    asyncio.run(run_main())
+scheduler.add_job(
+    run_async_job,
+    id="load_market_data",
+    args=[async_download_market_data],
+    # Run every four hours
+    trigger="interval",
+    hours=4,
+    replace_existing=True,
+)
