283cf70f7a163f42c99e281fe9958404dba860cd
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-12-27 17:33:14 +0100

    Layout improvements (#1033)

--- frontend/src/components/ChartPriceHistory.tsx
+++ frontend/src/components/ChartPriceHistory.tsx
@@ -338,7 +338,7 @@ function ChartPriceHistory({ isinCode }: { isinCode: string }) {
             isinCode={isinCode}
           />
           <StandardCard
-            height={"560px"}
+            height={"564px"}
             sx={{ marginTop: "5px", marginBottom: "0px", paddingLeft: "16px" }}
           >
             <Line
