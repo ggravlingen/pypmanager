76e16af7d96342cf6c54dcdd7efd3d0983ada3bd
Patrik <21142447+ggravlingen@users.noreply.github.com>
2024-12-29 10:54:41 +0000

    Add status endpoint

--- tests/api/test_endpoint.py
+++ tests/api/test_endpoint.py
@@ -4,6 +4,7 @@ from fastapi.testclient import TestClient
 import pytest
 
 from pypmanager.api import app
+from pypmanager.const import HttpStatusCodes
 
 client = TestClient(app)
 
@@ -12,11 +13,17 @@ client = TestClient(app)
 async def test_root_endpoint() -> None:
     """Test endpoint /."""
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == HttpStatusCodes.OK
 
 
 @pytest.mark.asyncio
 async def test_favicon() -> None:
     """Test endpoint /favicon.ico."""
     response = client.get("/favicon.ico")
-    assert response.status_code == 200
+    assert response.status_code == HttpStatusCodes.OK
+
+
+def test_endpoint_status() -> None:
+    """Test endpoint /status."""
+    response = client.get("/status")
+    assert response.status_code == HttpStatusCodes.OK
