152bf39424edda30f1c4c772a53b3e2214b5a5ec
Patrik <21142447+ggravlingen@users.noreply.github.com>
2025-01-01 19:43:38 +0000

    Remove unused code

--- pypmanager/analytics/__init__.py
+++ pypmanager/analytics/__init__.py
@@ -1,11 +1,9 @@
 """Analytics."""
 
-from .holding import Holding
 from .portfolio import Portfolio
 from .security import MutualFund
 
 __all__ = [
-    "Holding",
     "MutualFund",
     "Portfolio",
 ]
--- pypmanager/helpers/__init__.py
+++ pypmanager/helpers/__init__.py
@@ -1,11 +1,1 @@
 """Helpers."""
-
-from .portfolio import (
-    async_get_historical_portfolio,
-    async_get_holdings,
-)
-
-__all__ = [
-    "async_get_historical_portfolio",
-    "async_get_holdings",
-]
