efcfaa3614c993d126cf2fe92dd37dab77e573f4
Patrik <21142447+ggravlingen@users.noreply.github.com>
2025-01-01 20:03:17 +0000

    Prettier

--- frontend/src/Api/Models/transaction.tsx
+++ frontend/src/Api/Models/transaction.tsx
@@ -1,22 +1,22 @@
 export interface BaseTransactionRow {
-    transactionDate?: Date;
-    isinCode?: string;
-    broker?: string;
-    source?: string;
-    action?: string;
-    name?: string;
-    noTraded?: number;
-    commission?: number;
-    fx?: number;
-  }
+  transactionDate?: Date;
+  isinCode?: string;
+  broker?: string;
+  source?: string;
+  action?: string;
+  name?: string;
+  noTraded?: number;
+  commission?: number;
+  fx?: number;
+}
 
-  export interface TransactionRow extends BaseTransactionRow {
-    price?: number;
-    currency?: string;
-    cashFlow?: number;
-    costBaseAverage?: number;
-    quantityHeld?: number;
-    pnlTotal?: number;
-    pnlTrade?: number;
-    pnlDividend?: number;
-  }
+export interface TransactionRow extends BaseTransactionRow {
+  price?: number;
+  currency?: string;
+  cashFlow?: number;
+  costBaseAverage?: number;
+  quantityHeld?: number;
+  pnlTotal?: number;
+  pnlTrade?: number;
+  pnlDividend?: number;
+}
