df702c9fe4dac85f757c3df73581722fcd984832
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2025-01-01 20:49:02 +0100

    Remove unused code (#1094)

--- pypmanager/helpers/portfolio.py
+++ pypmanager/helpers/portfolio.py
@@ -3,54 +3,17 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from datetime import date, datetime
+from datetime import date  # noqa: TC003
 import logging
-from typing import cast
 
 import pandas as pd
 import strawberry
 
-from pypmanager.analytics.holding import Holding
-from pypmanager.analytics.portfolio import Portfolio
-from pypmanager.general_ledger import async_get_general_ledger
 from pypmanager.helpers.market_data import async_get_last_market_data_df
 from pypmanager.ingest.transaction.const import TransactionRegistryColNameValues
 from pypmanager.ingest.transaction.transaction_registry import TransactionRegistry
-from pypmanager.settings import Settings
-from pypmanager.utils.dt import async_get_last_n_quarters
 
 LOGGER = logging.getLogger(__package__)
-
-
-async def async_get_holdings(report_date: datetime | None = None) -> list[Holding]:
-    """
-    Return a list of current holdings.
-
-    Deprecated.
-    """
-    df_general_ledger = await async_get_general_ledger(report_date=report_date)
-    all_securities = cast(
-        list[str],
-        df_general_ledger[
-            TransactionRegistryColNameValues.SOURCE_NAME_SECURITY
-        ].unique(),
-    )
-
-    holdings: list[Holding] = []
-    for security_name in all_securities:
-        holding = Holding(
-            name=security_name,
-            df_general_ledger=df_general_ledger,
-            report_date=report_date,
-        )
-
-        if holding.total_pnl == 0:
-            continue
-
-        holdings.append(holding)
-
-    # Order by name
-    return sorted(holdings, key=lambda x: x.name)
 
 
 @dataclass
@@ -126,40 +89,3 @@ async def async_async_get_holdings_v2() -> list[Holdingv2]:
         )
 
     return sorted(output_data, key=lambda x: x.name)
-
-
-@dataclass
-class PortfolioSnapshot:
-    """A point of time snapshot of a portfolio."""
-
-    report_date: date
-    portfolio: Portfolio
-
-
-async def async_get_historical_portfolio() -> list[PortfolioSnapshot]:
-    """Return a list of historical portfolios."""
-    quarter_list = await async_get_last_n_quarters(no_quarters=8)
-    quarter_list.append(datetime.now(Settings.system_time_zone))
-
-    portfolio_data: list[PortfolioSnapshot] = []
-    for report_date in quarter_list:
-        _report_date = datetime(
-            report_date.year,
-            report_date.month,
-            report_date.day,
-            tzinfo=Settings.system_time_zone,
-        )
-        try:
-            holdings = await async_get_holdings(report_date=_report_date)
-        except ValueError:
-            continue
-
-        portfolio = Portfolio(holdings=holdings)
-        portfolio_data.append(
-            PortfolioSnapshot(
-                report_date=report_date,
-                portfolio=portfolio,
-            ),
-        )
-
-    return portfolio_data
