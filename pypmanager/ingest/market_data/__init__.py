e2fa17e250414305ce016c17e10ec054b437755a
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2025-01-03 10:47:21 +0100

    Add loader for Levler (#1137)

--- pypmanager/ingest/market_data/__init__.py
+++ pypmanager/ingest/market_data/__init__.py
@@ -2,11 +2,13 @@
 
 from .avanza import AvanzaLoader
 from .ft import FTLoader
+from .levler import LevlerLoader
 from .morningstar import MorningstarLoader, MorningstarLoaderSHB
 
 __all__ = [
     "AvanzaLoader",
     "FTLoader",
+    "LevlerLoader",
     "MorningstarLoader",
     "MorningstarLoaderSHB",
 ]
