5cf4a8371829dd3690f45ad9fef3c58ce48156cc
Patrik <21142447+ggravlingen@users.noreply.github.com>
2025-01-04 07:40:12 +0000

    Add tests for TransactionLoader.load_data_files

--- tests/ingest/transaction/test_base_loader.py
+++ tests/ingest/transaction/test_base_loader.py
@@ -3,10 +3,17 @@
 from __future__ import annotations
 
 from pathlib import Path
+from unittest.mock import patch
 
+import pandas as pd
 import pytest
 
-from pypmanager.ingest.transaction.base_loader import TransactionLoader, _get_filename
+from pypmanager.ingest.transaction.base_loader import (
+    EMPTY_DF,
+    TransactionLoader,
+    _get_filename,
+)
+from pypmanager.ingest.transaction.const import TransactionRegistryColNameValues
 
 
 @pytest.mark.parametrize(
@@ -37,3 +44,107 @@ def test_empty_loader() -> None:
     mock_loader = MockLoader()
 
     assert len(mock_loader.df_final) == 0
+
+
+@pytest.mark.parametrize(
+    ("files", "expected"),
+    [
+        (
+            ["file1.csv", "file2.csv"],
+            pd.DataFrame(
+                {
+                    "A": [1, 2, 3, 4],
+                    TransactionRegistryColNameValues.SOURCE_TRANSACTION_DATE.value: [
+                        "2021-01-01",
+                        "2021-01-02",
+                        "2021-01-03",
+                        "2021-01-04",
+                    ],
+                    TransactionRegistryColNameValues.SOURCE_FILE.value: [
+                        "testfile",
+                        "testfile",
+                        "testfile",
+                        "testfile",
+                    ],
+                }
+            ),
+        ),
+        (
+            ["file3.csv"],
+            pd.DataFrame(
+                {
+                    "B": [5, 6],
+                    TransactionRegistryColNameValues.SOURCE_TRANSACTION_DATE.value: [
+                        "2021-01-05",
+                        "2021-01-06",
+                    ],
+                    TransactionRegistryColNameValues.SOURCE_FILE.value: [
+                        "testfile",
+                        "testfile",
+                    ],
+                }
+            ),
+        ),
+        ([], EMPTY_DF),
+    ],
+)
+def test_load_data_files(files: list[str], expected: pd.DataFrame) -> None:
+    """Test the load_data_files method."""
+
+    class MockLoader(TransactionLoader):
+        """Mock the TransactionLoader."""
+
+        csv_separator = ","
+        file_pattern = "abc123"
+        date_format_pattern = "%Y-%m-%d"
+
+        def pre_process_df(self: MockLoader) -> None:
+            """Mock method."""
+
+    def mock_read_csv(file: Path, sep: str) -> pd.DataFrame:  # noqa: ARG001
+        if "file1.csv" in str(file):
+            return pd.DataFrame(
+                {
+                    "A": [1, 2],
+                    TransactionRegistryColNameValues.SOURCE_TRANSACTION_DATE.value: [
+                        "2021-01-01",
+                        "2021-01-02",
+                    ],
+                }
+            )
+        if "file2.csv" in str(file):
+            return pd.DataFrame(
+                {
+                    "A": [3, 4],
+                    TransactionRegistryColNameValues.SOURCE_TRANSACTION_DATE.value: [
+                        "2021-01-03",
+                        "2021-01-04",
+                    ],
+                }
+            )
+        if "file3.csv" in str(file):
+            return pd.DataFrame(
+                {
+                    "B": [5, 6],
+                    TransactionRegistryColNameValues.SOURCE_TRANSACTION_DATE.value: [
+                        "2021-01-05",
+                        "2021-01-06",
+                    ],
+                }
+            )
+
+        msg = f"Unexpected file: {file}"
+        raise ValueError(msg)
+
+    with (
+        patch("pandas.read_csv", side_effect=mock_read_csv),
+        patch("pathlib.Path.glob", return_value=[Path(f) for f in files]),
+        patch(
+            "pypmanager.ingest.transaction.base_loader._get_filename",
+            return_value="testfile",
+        ),
+    ):
+        loader = MockLoader()
+        loader.load_data_files()
+
+    pd.testing.assert_frame_equal(loader.df_final, expected)
