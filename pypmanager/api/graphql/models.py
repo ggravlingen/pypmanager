64896c230b4048c15d33db568ed238d13bbfaa6f
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2025-01-05 10:29:32 +0100

    Remove unused code (#1158)

--- pypmanager/api/graphql/models.py
+++ pypmanager/api/graphql/models.py
@@ -9,23 +9,6 @@ from pypmanager.helpers.security import Security
 
 
 @strawberry.type
-class PortfolioContentRow:
-    """Represent a row in the current portfolio content."""
-
-    name: str
-    date_market_value: str | None
-    invested_amount: float | None
-    market_value: float | None
-    current_holdings: float | None
-    current_price: float | None
-    average_price: float | None
-    return_pct: float | None
-    total_pnl: float
-    realized_pnl: float
-    unrealized_pnl: float
-
-
-@strawberry.type
 class ResultStatementRow:
     """Represent a row in the result statement."""
 
