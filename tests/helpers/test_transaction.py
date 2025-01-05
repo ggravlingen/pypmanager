5b9810ba9c838fb6d4156f60305e92cde7804f44
“Patrik <21142447+ggravlingen@users.noreply.github.com>
2025-01-05 09:41:48 +0000

    Improve tests

--- tests/helpers/test_transaction.py
+++ tests/helpers/test_transaction.py
@@ -16,16 +16,27 @@ if TYPE_CHECKING:
 
 
 @pytest.mark.asyncio
-async def test_async_get_all_transactions(
+async def test_async_get_all_transactions(  # noqa: PLR0915
     data_factory: type[DataFactory],
 ) -> None:
     """Test async_get_all_transactions."""
     factory = data_factory()
     mocked_transactions = (
         factory.buy(
-            transaction_date=datetime(2022, 11, 1, tzinfo=Settings.system_time_zone)
+            # Security A
+            transaction_date=datetime(2022, 11, 1, tzinfo=Settings.system_time_zone),
+            commission=None,
         )
-        .sell(transaction_date=datetime(2022, 11, 2, tzinfo=Settings.system_time_zone))
+        .sell(
+            # Security A
+            transaction_date=datetime(2022, 11, 2, tzinfo=Settings.system_time_zone)
+        )
+        .buy(
+            # Security B
+            name="Security B",
+            transaction_date=datetime(2022, 11, 3, tzinfo=Settings.system_time_zone),
+            isin_code=None,
+        )
         .df_transaction_list
     )
     with (
@@ -36,40 +47,65 @@ async def test_async_get_all_transactions(
         ),
     ):
         result = await async_get_all_transactions()
+
+        assert len(result) == 3
+
         assert result[0].transaction_date == datetime(
-            2022, 11, 2, tzinfo=Settings.system_time_zone
+            2022, 11, 3, tzinfo=Settings.system_time_zone
         )
         assert result[0].broker == "Broker A"
-        assert result[0].action == "Sell"
-        assert result[0].name == "Company A"
+        assert result[0].source == "test-file"
+        assert result[0].action == "Buy"
+        assert result[0].name == "Security B"
         assert result[0].no_traded == 10.0
         assert result[0].commission == -1.0
         assert result[0].fx == 1.0
-        assert result[0].isin_code == "US1234567890"
-        assert result[0].price == 15.0
+        assert result[0].isin_code is None
+        assert result[0].price == 10.0
         assert result[0].currency == "SEK"
-        assert result[0].cash_flow == 149.0
-        assert result[0].cost_base_average is None
-        assert result[0].pnl_total == 49.0
-        assert result[0].pnl_trade == 49.0
+        assert result[0].cash_flow == -101.0
+        assert result[0].cost_base_average == 10.0
+        assert result[0].pnl_total == 0.0
+        assert result[0].pnl_trade is None
         assert result[0].pnl_dividend is None
-        assert result[0].quantity_held is None
+        assert result[0].quantity_held == 10.0
 
         assert result[1].transaction_date == datetime(
-            2022, 11, 1, tzinfo=Settings.system_time_zone
+            2022, 11, 2, tzinfo=Settings.system_time_zone
         )
         assert result[1].broker == "Broker A"
-        assert result[1].action == "Buy"
+        assert result[1].source == "test-file"
+        assert result[1].action == "Sell"
         assert result[1].name == "Company A"
         assert result[1].no_traded == 10.0
         assert result[1].commission == -1.0
         assert result[1].fx == 1.0
         assert result[1].isin_code == "US1234567890"
-        assert result[1].price == 10.0
+        assert result[1].price == 15.0
         assert result[1].currency == "SEK"
-        assert result[1].cash_flow == -101.0
-        assert result[1].cost_base_average == 10.0
-        assert result[1].pnl_total == 0.0
-        assert result[1].pnl_trade is None
+        assert result[1].cash_flow == 149.0
+        assert result[1].cost_base_average is None
+        assert result[1].pnl_total == 49.0
+        assert result[1].pnl_trade == 49.0
         assert result[1].pnl_dividend is None
-        assert result[1].quantity_held == 10.0
+        assert result[1].quantity_held is None
+
+        assert result[2].transaction_date == datetime(
+            2022, 11, 1, tzinfo=Settings.system_time_zone
+        )
+        assert result[2].broker == "Broker A"
+        assert result[2].source == "test-file"
+        assert result[2].action == "Buy"
+        assert result[2].name == "Company A"
+        assert result[2].no_traded == 10.0
+        assert result[2].commission is None
+        assert result[2].fx == 1.0
+        assert result[2].isin_code == "US1234567890"
+        assert result[2].price == 10.0
+        assert result[2].currency == "SEK"
+        assert result[2].cash_flow is None
+        assert result[2].cost_base_average == 10.0
+        assert result[2].pnl_total == 0.0
+        assert result[2].pnl_trade is None
+        assert result[2].pnl_dividend is None
+        assert result[2].quantity_held == 10.0
