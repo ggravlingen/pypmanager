136509335bccbe29791c368da2eaa734f1dc5bf1
“Patrik <21142447+ggravlingen@users.noreply.github.com>
2025-01-06 06:02:16 +0000

    Update copilot

--- pypmanager/helpers/__init__.py
+++ pypmanager/helpers/__init__.py
@@ -1,12 +1,4 @@
-"""
-Helpers.
-
-Naming conventions:
-- A function that returns a dictionary should be named map and then what it maps from
-    and to. For example, async_map_isin_to_pnl_data, which maps an ISIN code to a
-    PnLData object.
-
-"""
+"""Helpers."""
 
 from .income_statement import (
     ResultStatementRow,
