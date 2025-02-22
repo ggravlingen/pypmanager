2ae78d757bd3bdce320e00fb7ccd3b2100f59732
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2025-02-22 08:27:55 +0100

    Use requests session (#1297)

--- tests/ingest/market_data/test_avanza.py
+++ tests/ingest/market_data/test_avanza.py
@@ -16,7 +16,7 @@ def test_avanza_loader() -> None:
         "name": "Sample Fund",
     }
     with mock.patch(
-        "pypmanager.ingest.market_data.base_loader.requests.get",
+        "pypmanager.ingest.market_data.base_loader.requests.Session.get",
     ) as mock_get:
         mock_response = mock_get.return_value
         mock_response.status_code = 200
