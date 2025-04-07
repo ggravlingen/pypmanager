a982f0dc04279bd6aa488c1c63d1a32097f6293f
“Patrik <21142447+ggravlingen@users.noreply.github.com>
2025-04-07 18:53:12 +0000

    Add AsyncMarketDataDB.get_market_data()

--- tests/database/test_market_data.py
+++ tests/database/test_market_data.py
@@ -78,3 +78,25 @@ async def test_store_market_data(
     async with AsyncMarketDataDB() as db:
         await db.store_market_data(data=sample_market_data)
         assert "Stored 2 market data records" in caplog.text
+
+
+@pytest.mark.asyncio
+async def test_get_market_data(
+    sample_market_data: list[MarketDataModel],
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    """Test method get_market_data."""
+    async with AsyncMarketDataDB() as db:
+        await db.store_market_data(data=sample_market_data)
+        assert "Stored 2 market data records" in caplog.text
+
+        data = await db.get_market_data(
+            isin="US0378331005",
+            report_date=date(2023, 1, 1),
+        )
+
+        assert data is not None
+        assert data.isin == "US0378331005"
+        assert data.close_price == 150.25
+        assert data.report_date == date(2023, 1, 1)
+        assert data.source == "test"
