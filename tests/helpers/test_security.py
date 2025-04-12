1d4f0e1170bb5de411b0a1a83164c5ae2e40d5f6
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2025-04-12 21:06:41 +0200

    Use database in async_security_map_isin_to_security (#1422)

--- tests/helpers/test_security.py
+++ tests/helpers/test_security.py
@@ -11,21 +11,21 @@ from pypmanager.helpers.security import async_security_map_name_to_isin
 
 
 @pytest.mark.asyncio
-@pytest.mark.usefixtures("mock_file_security_config_local")
+@pytest.mark.usefixtures("load_security_data")
 async def test_async_load_security_data() -> None:
     """Test async_load_security_data."""
     result = await async_security_map_isin_to_security()
 
-    assert len(result) == 2
+    assert len(result) == 1
     assert result.get("SE0005188836").name == "Länsförsäkringar Global Index"
     assert result.get("SE0005188836").isin_code == "SE0005188836"
     assert result.get("SE0005188836").currency == "SEK"
 
 
 @pytest.mark.asyncio
-@pytest.mark.usefixtures("mock_file_security_config_local")
+@pytest.mark.usefixtures("load_security_data")
 async def test_async_security_map_name_to_isin() -> None:
     """Test function async_security_map_name_to_isin."""
     result = await async_security_map_name_to_isin()
-    assert len(result) == 2
+    assert len(result) == 1
     assert result.get("Länsförsäkringar Global Index") == "SE0005188836"
